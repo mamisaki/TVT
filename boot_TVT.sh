7d794cd30b858830c29f559715cb38e1570fc782
hika <h.kuroshima@gmail.com>
2024-06-30 14:53:47 +0900

    update

--- boot_TVT.sh
+++ boot_TVT.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [ -d $HOME/anaconda3 ]; then
+  CONDA_DIR=$HOME/anaconda3
+fi
+
+if [ -d $HOME/miniconda3 ]; then
+  CONDA_DIR=$HOME/miniconda3
+fi
+
 # anaconda environment setup
-. $HOME/anaconda3/etc/profile.d/conda.sh
+. $CONDA_DIR/etc/profile.d/conda.sh
 conda activate TVT
 
 cd $HOME/TVT/code
